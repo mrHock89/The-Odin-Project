df39b2fdce566d58cd01d03f4b312b13f0bf7e64
mrHock89 <147297458+mrHock89@users.noreply.github.com>
2025-01-03 10:00:59 +0530

    even number

--- JavaScript/array.js
+++ JavaScript/array.js
@@ -44,3 +44,56 @@ console.log(Array.isArray(points)); // will return true ia it is an array
 // 2
 const fruits = ["Banana", "Apple", "Orange"];
 console.log(fruits instanceof Array);
+
+// Nested Arrays and objects
+console.log("Nested Arrays and Objects");
+let myObj = new Object();
+myObj = {
+  name: "Anirban",
+  age: 24,
+  friends: ["Ani", "Happy", "Herry"],
+};
+
+// Accessing object elements
+console.log(myObj.age);
+console.log(myObj["age"]);
+console.log(myObj.name);
+
+// Accessing the array of the Object
+console.log(Array.isArray(myObj.friends)); // checks if is it array or not
+console.log(`Array elements of the object are: ${myObj.friends}`);
+
+const myArr = [
+  [1, 2],
+  [4, 3],
+  [7, 6],
+];
+console.log(myArr);
+const newArr = myArr.flat();
+console.log(newArr);
+
+const newArr2 = newArr.flatMap((x) => [x, x * 10]);
+const newArr3 = newArr.flatMap((x) => [x * 10]);
+console.log(newArr2);
+console.log(newArr3);
+
+const fruits2 = ["Banana", "Apple", "Ornge", "Mango"];
+fruits2.splice(2, 1, "Lemon", "Kiwi");
+console.log(fruits2);
+
+const fruits3 = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(0, 1);
+console.log(fruits3);
+
+const newFArr = fruits3.slice(2);
+const newFArr2 = fruits3.slice(0, 2);
+console.log(newFArr);
+console.log(newFArr2);
+console.log(fruits3);
+// let i = 3;
+// while (i) {
+//   console.log(i--); // 3 2 1
+// }
+// let i = 0;
+// while (i++ < 5) console.log(i); // 1 2 3 4
+for (let i = 0; i < 5; ++i) console.log(i);
