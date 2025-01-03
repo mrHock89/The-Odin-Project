6b54b06ba8531e9058a578077a803bcc405d556c
mrHock89 <147297458+mrHock89@users.noreply.github.com>
2025-01-03 10:57:45 +0530

    prime number

---
+++ JavaScript/prime_number.js
@@ -0,0 +1,15 @@
+const lastVal = 15;
+
+for (let i = 2; i <= lastVal; i++) {
+  let isPrime = true;
+  for (let j = 2; j < i; j++) {
+    if (i % j == 0) {
+      //   continue;
+      isPrime = false;
+      break;
+    }
+  }
+  if (isPrime) {
+    console.log(i);
+  }
+}
