0ae938255be5e7cb9c349aede21cff5f794514db
mrHock89 <147297458+mrHock89@users.noreply.github.com>
2025-01-05 09:36:55 +0530

    more optimize

--- DOM/Event/script.js
+++ DOM/Event/script.js
@@ -1,7 +1,28 @@
-const btn = document.querySelector("#btn");
-btn.onclick = () => alert("Hello World!");
+// const btn = document.querySelector("#btn");
+// btn.onclick = () => alert("Hello World!");
 
+// const btn2 = document.querySelector("#btn2");
+// btn2.addEventListener("click", () => {
+//   alert("Hello World!");
+// });
+
+// btn 1
+function alertFunction() {
+  alert("Yay! you dit it!");
+}
+
+function alertFunction2() {
+  alert("Yay! you did it again!");
+}
+// btn 2
+const btn = document.querySelector("#btn");
+btn.onclick = alertFunction2;
+
+// btn 3
 const btn2 = document.querySelector("#btn2");
-btn2.addEventListener("click", () => {
-  alert("Hello World!");
+// btn2.addEventListener("click", alertFunction2);
+
+btn2.addEventListener("click", function (e) {
+  //   console.log(e.target);
+  e.target.style.background = "blue";
 });
