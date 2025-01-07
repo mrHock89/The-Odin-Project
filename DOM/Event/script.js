be7bddf1b89eccf844ba44534ba8471624d7317f
mrHock89 <147297458+mrHock89@users.noreply.github.com>
2025-01-07 08:50:27 +0530

    learning event in js

--- DOM/Event/script.js
+++ DOM/Event/script.js
@@ -7,22 +7,29 @@
 // });
 
 // btn 1
-function alertFunction() {
-  alert("Yay! you dit it!");
-}
+// function alertFunction() {
+//   alert("Yay! you dit it!");
+// }
 
-function alertFunction2() {
-  alert("Yay! you did it again!");
-}
-// btn 2
-const btn = document.querySelector("#btn");
-btn.onclick = alertFunction2;
+// function alertFunction2() {
+//   alert("Yay! you did it again!");
+// }
+// // btn 2
+// const btn = document.querySelector("#btn");
+// btn.onclick = alertFunction2;
 
-// btn 3
-const btn2 = document.querySelector("#btn2");
-// btn2.addEventListener("click", alertFunction2);
+// // btn 3
+// const btn2 = document.querySelector("#btn2");
+// // btn2.addEventListener("click", alertFunction2);
 
-btn2.addEventListener("click", function (e) {
-  //   console.log(e.target);
-  e.target.style.background = "blue";
+// btn2.addEventListener("click", function (e) {
+//   //   console.log(e.target);
+//   e.target.style.background = "blue";
+// });
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
 });
